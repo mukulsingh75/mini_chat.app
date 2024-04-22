9c06bb7d36f998e9c6a1f9d9acf923cdeee2a3b6
mukulsingh75 <mukulsingh.cse@gmail.com>
2024-04-22 11:21:29 +0530

    implement error handling

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat.js");
 const methodOverride = require("method-override");
+const ExpressError = require("./ExpressError.js");
 
 
 app.use(methodOverride("_method"));
@@ -19,7 +20,7 @@ app.set("view engine","ejs");
 
 main()
   .then((res) => {
-    console.log(res);
+    console.log("connection establish");
   })
   .catch((err) => console.log(err));
 
@@ -44,72 +45,112 @@ async function main(){      //connection establish
 
    // get route
 
-app.get("/chats", async (req,res)=>{
-    let chats = await Chat.find();
+app.get("/chats", asyncWrap( async (req,res,err)=>{    //here using async wrap
+        let chats = await Chat.find();
     // console.log(chats);
     res.render("index.ejs",{chats});
-})
+}
+));
 
     // new route
 
 app.get("/chats/new",(req,res)=>{
+    // throw new ExpressError(404,"page not found");
     res.render("new.ejs");
 })
 
    //create route
 
-app.post("/chats",(req,res)=>{
-    let{from,msg,to} = req.body;
+app.post("/chats",asyncWrap(async (req, res, next) => {
+    let { from, msg, to } = req.body;
     let newChat = new Chat({
-        from:from,
-        to:to,
-        msg:msg,
-        created_at : new Date()
+      from: from,
+      to: to,
+      msg: msg,
+      created_at: new Date()
     });
-    newChat.save().then((res)=>{
-        console.log("chat saved");
-    })
-    .catch((err)=>{
-        console.log(err);
-    })
+    await newChat.save();
     res.redirect("/chats");
+  })
+);
 
-})
+function asyncWrap(fn){
+    return function(req,res,next){
+        fn(req,res,next).catch((err) => next(err));
+    }
+}
+
+     //show route
+
+app.get("/chats/:id",asyncWrap(async(req,res,next)=>{
+        let {id} = req.params;
+        let chat = await Chat.findById(id);
+        if(!chat){
+            next(new ExpressError(500,"chat not found")) ;
+        }
+        res.render("edit.ejs",{chat});
+    }
+));     
 
       //edit route
 
-app.get("/chats/:id/edit",async(req,res)=>{
-    let {id} = req.params;
+app.get("/chats/:id/edit",asyncWrap(async (req, res, next) => {
+    let { id } = req.params;
     let chat = await Chat.findById(id);
-    res.render("edit.ejs",{chat});
-})  
+    res.render("edit.ejs", { chat });
+  })
+);  
 
        //update route
 
-app.put("/chats/:id", async (req, res) => {
-  let { id } = req.params;
-  let { msg:newmsg } = req.body;
-  let updatedChat = await Chat.findByIdAndUpdate(
-    id,
-    { msg: newmsg,updated_at:new Date() },
-    { runValidators: true, new: true }
-  );
-  console.log(updatedChat);
-  res.redirect("/chats");
-});     
+app.put("/chats/:id", asyncWrap(async (req, res,next) => {
+    let { id } = req.params;
+    let { msg: newmsg } = req.body;
+    let updatedChat = await Chat.findByIdAndUpdate(
+      id,
+      { msg: newmsg, updated_at: new Date() },
+      { runValidators: true, new: true }
+    );
+    console.log(updatedChat);
+    res.redirect("/chats"); 
+})
+);     
 
 
        //delete button
 
-app.delete("/chats/:id/delete",async(req,res)=>{
-    let {id} = req.params;
+app.delete("/chats/:id/delete", asyncWrap(async (req, res, err) => {
+    let { id } = req.params;
     let delChat = await Chat.findByIdAndDelete(id);
     console.log(delChat);
     res.redirect("/chats");
-})       
+  })
+);       
 
 app.get("/",(req,res)=>{
     res.send("Working");
+})
+
+const handleValidationErr = (err)=>{
+    console.log("This was a Validation error. Please follow rules");
+    console.dir(err.message);
+    return err;
+}
+
+app.use((err,req,res,next)=>{
+    console.log(err.name);
+    if(err.name === 'ValidationError'){
+        handleValidationErr(err);
+    }
+    next(err);
+
+})
+
+//error handling middleware
+
+app.use((err,req,res,next)=>{
+    let {status=500,message="some error occured"} = err;
+    res.status(status).send(message);
 })
 
 
